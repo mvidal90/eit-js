ee5bb695a087861133834edf9b4c2f0b259c1dc9
Marcos Vidal <devtionit@gamil.com>
2025-03-13 21:32:03 -0300

    ADD example multi for staments

--- prompt-bucles.js
+++ prompt-bucles.js
@@ -1,22 +1,35 @@
 
-const saludo = window.prompt("Escriba un saludo:")
+// const saludo = window.prompt("Escriba un saludo:")
 
-const alumno = " Alumno nro. " 
-// index++ operador acumulador
-for (let index = 0; index < 10; index++) {
-    if (saludo !== null) {
-        console.log(
-            saludo.concat(
-                alumno.concat(index)
-            )
-        )
+// const alumno = " Alumno nro. " 
+// // index++ operador acumulador
+// for (let index = 0; index < 10; index++) {
+//     if (saludo !== null) {
+//         console.log(
+//             saludo.concat(
+//                 alumno.concat(index)
+//             )
+//         )
+//     }
+// }
+
+// let response;
+
+// while (response !== "8") {
+//     response = window.prompt("Adivine el número entre 0 y 20:")
+// }
+
+// console.log("Has adivinado el número")
+
+// Tenemos 10 cursos (EL primer curso arranca en 1) donde cada curso tiene 20 alumnos
+// Imprimir en consola cada curso con sus alumnos
+
+for (let course = 1; course <= 10 ; course++) {
+    console.log(`Curso nro. ${course}`) //Template string
+    for (let child = 1; child <= 15; child++) {
+        console.log(`Alumno nro. ${child}`)
     }
 }
 
-let response;
-
-while (response !== "8") {
-    response = window.prompt("Adivine el número entre 0 y 20:")
-}
-
-console.log("Has adivinado el número")
+const a = 99
+console.log(`${ a < 100 ? "0" : "" }${a}`) // Condicional Ternario (caso true y caso false)
